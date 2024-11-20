974a12106b65a53089e78b164cf80f973da6b982
Kian Kolahdoozan <kianmars60@gmail.com>
2024-11-20 13:15:18 +0100

    check names for friends and enemies

--- WeddingSolverFinal/Program.cs
+++ WeddingSolverFinal/Program.cs
@@ -30,10 +30,18 @@
             throw new ArgumentException("The number of guests exceeds the total number of available seats.");
         }
     }
-
-    public void AddFriend(string name1, string name2){
+    public void AddFriend(string name1, string name2)
+    {
         int index1 = guests.IndexOf(name1);
         int index2 = guests.IndexOf(name2);
+        if (index1 == -1)
+        {
+            throw new Exception($"The name {name1} is not in the guests list.");
+        }
+        else if (index2 == -1)
+        {
+            throw new Exception($"The name {name2} is not in the guests list.");
+        }
         relationshipMatrix[index1, index2] = 1;
         relationshipMatrix[index2, index1] = 1;
     }
@@ -41,6 +49,14 @@
     {
         int index1 = guests.IndexOf(name1);
         int index2 = guests.IndexOf(name2);
+        if (index1 == -1)
+        {
+            throw new Exception($"The name {name1} is not in the guests list.");
+        }
+        else if (index2 == -1)
+        {
+            throw new Exception($"The name {name2} is not in the guests list.");
+        }
         relationshipMatrix[index1, index2] = -1;
         relationshipMatrix[index2, index1] = -1;
     }
@@ -64,29 +80,39 @@
         {
             if (guest.Item2 > seatsPerTable - 1)
             {
-                int guestIndex = guests.IndexOf(guest.Item1);
-                bool hasEnemiesEverywhere = true;
-                for (int table = 0; table < numberTables; table++)
+
+                int amountFriends = 0;
+                for (int i = 0; i < guests.Count; i++)
                 {
-                    if (IsSeatAvailable(table, guestIndex))
+                    if (relationshipMatrix[guests.IndexOf(guest.Item1), i] == 1)
                     {
-                        hasEnemiesEverywhere = false;
-                        break;
+                        amountFriends++;
                     }
                 }
-                
-                if (hasEnemiesEverywhere)
+                if (amountFriends > seatsPerTable - 1)
+                {
+                    throw new InvalidOperationException($"Guest {guest.Item1} has more desired seating companions than the available seats per table.");
+                }
+
+                int guestIndex = guests.IndexOf(guest.Item1);
+                if (!CanBeSeated(guestIndex))
                 {
                     throw new InvalidOperationException($"Guest {guest.Item1} cannot be seated as they have an enemy at every table.");
                 }
-                else throw new InvalidOperationException($"Guest {guest.Item1} has more desired seating companions than the available seats per table.");
-                
             }
-            
+
         }
 
         if (!AssignSeatingHelper(orderedGuests, 0))
         {
+            foreach (var guest in orderedGuests)
+            {
+                int guestIndex = guests.IndexOf(guest.Item1);
+                if (!CanBeSeated(guestIndex))
+                {
+                    throw new InvalidOperationException($"Guest {guest.Item1} cannot be seated as they have an enemy at every table.");
+                }
+            }
             throw new InvalidOperationException("Unable to seat all guests according to the given relationships.");
         }
     }
@@ -115,7 +141,6 @@
         }
         return false; // No valid seating found for this configuration.
     }
-
     private bool IsSeatAvailable(int table, int guestIndex)
     {
         if (tables[table].Count >= seatsPerTable)
@@ -132,6 +157,27 @@
             }
         }
         return true;
+    }
+    private bool CanBeSeated(int guestIndex)
+    {
+        for (int table = 0; table < numberTables; table++)
+        {
+            bool hasEnemy = false;
+            foreach (string seatedGuest in tables[table])
+            {
+                int seatedIndex = guests.IndexOf(seatedGuest);
+                if (relationshipMatrix[guestIndex, seatedIndex] == -1)
+                {
+                    hasEnemy = true;
+                    break;
+                }
+            }
+            if (!hasEnemy) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<(string, int)> FindInfluencer()
@@ -158,8 +204,7 @@
         {
             Console.WriteLine($"{guest.Guest}: {guest.Count} relationships");
         }*/
-        return orderedGuests;
-        
+        return orderedGuests;     
     }
 
     public void PrintTables()
@@ -173,33 +218,35 @@
     public static void Main()
     {
         List<string> guest = new List<string> {"Tom", "Anna", "Daniel", "John", "Mia", "Jeff", "Clara", "Roxy", "Mark", "Maria", "Bella", "Marco", "Mustafa", "Li", "Peter", "Emma", "Thomas"};
-        WeddingSeatingSolver solver = new WeddingSeatingSolver(4, 5, guest);
-        solver.AddFriend("Tom", "Daniel");
-        solver.AddFriend("Tom", "John");
-        solver.AddFriend("Tom", "Mia");
-        //solver.AddFriend("Tom", "Clara");
-        //solver.AddFriend("Tom", "Jeff");
-        //solver.AddFriend("Tom", "Anna");
-        solver.AddEnemy("Marco", "Tom");
-        solver.AddEnemy("Marco", "Anna");
-        solver.AddEnemy("Marco", "Daniel");
-        solver.AddEnemy("Marco", "John");
-        solver.AddEnemy("Marco", "Mia");
-        solver.AddEnemy("Marco", "Jeff");
-        solver.AddEnemy("Marco", "Clara");
-        solver.AddEnemy("Marco", "Roxy");
-        solver.AddEnemy("Marco", "Mark");
-        solver.AddEnemy("Marco", "Maria");
-        solver.AddEnemy("Marco", "Bella");
-        solver.AddEnemy("Marco", "Mustafa");
-        solver.AddEnemy("Marco", "Li");
-        solver.AddEnemy("Marco", "Peter");
-        solver.AddEnemy("Marco", "Emma");
-        solver.AddEnemy("Marco", "Thomas");
-        solver.FindInfluencer();
-        // solver.DisplayRelationshipMatrix();
         try
         {
+            WeddingSeatingSolver solver = new WeddingSeatingSolver(4, 5, guest);
+            solver.AddFriend("Tom", "Daniel");
+            solver.AddFriend("Tom", "John");
+            solver.AddFriend("Tom", "Mia");
+            solver.AddFriend("Tom", "Clara");
+            solver.AddFriend("Clara", "Li");
+            solver.AddFriend("Emma", "Thomas");
+            solver.AddFriend("Roxy", "Bella");
+            //solver.AddFriend("Tom", "Jeff");
+            solver.AddEnemy("Marco", "Tom");
+            solver.AddEnemy("Marco", "Anna");
+            solver.AddEnemy("Marco", "Daniel");
+            solver.AddEnemy("Marco", "John");
+            solver.AddEnemy("Marco", "Mia");
+            solver.AddEnemy("Marco", "Jeff");
+            solver.AddEnemy("Marco", "Clara");
+            solver.AddEnemy("Marco", "Roxy");
+            //solver.AddEnemy("Marco", "Mark");
+            //solver.AddEnemy("Marco", "Maria");
+            solver.AddEnemy("Marco", "Bella");
+            solver.AddEnemy("Marco", "Mustafa");
+            solver.AddEnemy("Marco", "Li");
+            solver.AddEnemy("Marco", "Peter");
+            solver.AddEnemy("Marco", "Emma");
+            solver.AddEnemy("Marco", "Thomas");
+            solver.FindInfluencer();
+            // solver.DisplayRelationshipMatrix();
             solver.AssignSeating();
             solver.PrintTables();
         }
